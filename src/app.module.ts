ab2ee211080b5f4d1dee21f68371104a0cf39c92
vhfantes-Dev <167342932+vhfantes-Dev@users.noreply.github.com>
2025-10-08 00:03:37 -0300

    :zap: att. adiciona módulos e serviços ao AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { KuantokustaModule } from './kuantokusta/kuantokusta.module';
+import { ShopifyModule } from './shopify/shopify.module';
+import { SyncService } from './sync/sync.service';
+import { SyncController } from './sync/sync.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [KuantokustaModule, ShopifyModule,ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [AppController, SyncController],
+  providers: [AppService, SyncService],
 })
 export class AppModule {}
